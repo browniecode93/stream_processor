f56abdd1d8c8fdb054e2e9b653bc3e71f2af2678
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2021-11-21 17:08:50 +0330

    create card table

--- streams/models.py
+++ streams/models.py
@@ -1,6 +1,8 @@
 import faust
 
 class Card(faust.Record):
+    id: str = None
+    transaction_time: str = None
     request_code: int = None
     card_no: str = None
     account_number: str = None
@@ -9,3 +11,6 @@ class Card(faust.Record):
     reference_id: str = None
     amount: str = None
     occurrence_id: str = None
+    response_code: str = None
+    credit_name: str = None
+    debit_name: str = None
--- streams/transaction.py
+++ streams/transaction.py
@@ -1,7 +1,11 @@
 import logging
+import datetime
+from dateutil import tz
+import uuid
 
 import faust
 
+from streams.models import Card
 import streams.settings as settings
 
 logger = logging.getLogger(__name__)
@@ -21,12 +25,36 @@ app = faust.App(
 )
 
 transaction = app.topic(settings.TOPIC_TRANSACTIONS, acks=settings.OFFSET_ACK_ON_KAFKA, partitions=None)
+card_table = app.Table('card_table', default=Card)
 
-print("hereeeeeeeeee")
+from_zone = tz.tzutc()
+to_zone = tz.tzlocal()
 
 @app.agent(transaction)
 async def transactions(stream):
     async for event in stream:
-        print(f'>>>>>>>>>>>>>> {str(event)}')
         logger.info("Start process: " + str(event))
-        # Write you code here #
+
+        request_body = event['fullDocument']['RequestBody']
+        response_body = event['fullDocument']['ResponseBody']
+
+        transaction_date = event['create_at']
+        utc = datetime.datetime.fromtimestamp(transaction_date)
+        utc = utc.replace(tzinfo=from_zone)
+        transaction_time = utc.astimezone(to_zone)
+        id=uuid.uuid1()
+        card = Card(id=id,
+                    transaction_time=transaction_time,
+                    request_code=request_body['request_code'],
+                    card_no=request_body['note']['pan'],
+                    account_number=request_body['accountNumber'],
+                    terminal_id=request_body['note']['terminal'],
+                    terminal_type=request_body['note']['terminalType'],
+                    reference_id=request_body['note']['reference_id'],
+                    amount=request_body['transactionAmount'],
+                    occurrence_id=request_body['occurrenceId'],
+                    response_code=response_body['ResponseCode'],
+                    credit_name=response_body['toCardHolderName'],
+                    debit_name=response_body['fromCardHolderName'])
+        card_table[id] = card
+        print(f">>>>>>>>>>>> {card}")
