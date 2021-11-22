584bfcb03839fe0ed64470b1bd4542086f263e70
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2021-11-22 18:00:30 +0330

    add core processor, change producers

---
+++ streams/producer-core.py
@@ -0,0 +1,30 @@
+import json
+import faust
+
+import settings
+
+import asyncio
+import random
+from faust.cli import option
+
+app = faust.App('faust_core_producer', broker=settings.KAFKA_BOOTSTRAP_SERVER, web_port=7001)
+topic = app.topic('core_transaction')
+
+@app.command(
+    option('--max-latency',
+           type=float, default=0.5, envvar='PRODUCE_LATENCY',
+           help='Add delay of (at most) n seconds between publishing.')
+)
+async def produce(self, max_latency: float):
+    print("hereee")
+    """Produce example Withdrawal events."""
+    with open('events/test_c.json', 'r') as card_events:
+        data = json.load(card_events)
+    for dt in data:
+        print(dt)
+        await topic.send(value=dt)
+        if max_latency:
+            await asyncio.sleep(random.uniform(0, max_latency))
+
+if __name__ == '__main__':
+    app.main()
--- streams/transaction.py
+++ streams/transaction.py
@@ -2,11 +2,12 @@ import logging
 import datetime
 from dateutil import tz
 import uuid
-
 import faust
 
-from streams.models import Card
+from streams.models import Card, Core
 import streams.settings as settings
+import datetime
+
 
 logger = logging.getLogger(__name__)
 
@@ -24,25 +25,30 @@ app = faust.App(
     logging_config=settings.LOGGING
 )
 
-transaction = app.topic(settings.TOPIC_TRANSACTIONS, acks=settings.OFFSET_ACK_ON_KAFKA, partitions=None)
+transaction = app.topic(settings.TOPIC_TRANSACTIONS, acks=settings.OFFSET_ACK_ON_KAFKA, partitions=8)
 card_table = app.Table('card_table', default=Card)
+
+core = app.topic('core_transaction', acks=settings.OFFSET_ACK_ON_KAFKA, partitions=8)
+core_table = app.Table('core_table', default=Core)
 
 from_zone = tz.tzutc()
 to_zone = tz.tzlocal()
+
 
 @app.agent(transaction)
 async def transactions(stream):
     async for event in stream:
         logger.info("Start process: " + str(event))
-
         request_body = event['fullDocument']['RequestBody']
         response_body = event['fullDocument']['ResponseBody']
 
-        transaction_date = event['create_at']
+        transaction_date = event['fullDocument']['create_at']
         utc = datetime.datetime.fromtimestamp(transaction_date)
         utc = utc.replace(tzinfo=from_zone)
         transaction_time = utc.astimezone(to_zone)
+
         id=uuid.uuid1()
+
         card = Card(id=id,
                     transaction_time=transaction_time,
                     request_code=request_body['request_code'],
@@ -57,4 +63,40 @@ async def transactions(stream):
                     credit_name=response_body['toCardHolderName'],
                     debit_name=response_body['fromCardHolderName'])
         card_table[id] = card
-        print(f">>>>>>>>>>>> {card}")
+        print(f"{repr([k for (k,v) in card_table.items()])}")
+
+
+@app.agent(core)
+async def core(stream):
+    async for event in stream:
+        logger.info("Start process: " + str(event))
+        core_after = event['after']
+        if event['op_type'] == 'I':
+            transaction_date = core_after['transaction_date']
+            utc = datetime.datetime.fromtimestamp(transaction_date)
+            utc = utc.replace(tzinfo=from_zone)
+            transaction_time = utc.astimezone(to_zone)
+            id = core_after['id']
+            core_obj = Core(id=id,
+                        transaction_type=core_after['transaction_type_enum'],
+                        account_id=core_after['account_id'],
+                        occurrence_id=core_after['occurrence_id'],
+                        status=core_after['status'],
+                        transaction_code=core_after['transaction_code'],
+                        current_balance=core_after['current_balance'],
+                        transaction_date=transaction_time)
+            core_table[id] = core_obj
+            print(f"{repr([k for (k, v) in core_table.items()])}")
+
+        if event['op_type'] == 'U':
+            core_after = event['after']
+            id_u = core_after['id']
+            list_of_values = core_table[id_u].value()
+            list_of_values['transaction_code'] = core_after['transaction_code'],
+            list_of_values['current_balance'] = core_after['current_balance'],
+            list_of_values['status'] = core_after['status']
+            core_table[id_u] = list_of_values
+            print(f"{repr([k for (k, v) in core_table.items()])}")
+
+
+
