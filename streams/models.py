701639fb3dd2244fa5724116c924544212826f18
Farzaneh <farzaneh@Farzanehs-MacBook-Pro.local>
2021-11-26 12:50:11 +0330

    update models

--- streams/models.py
+++ streams/models.py
@@ -1,5 +1,6 @@
 import faust
 
+
 class Card(faust.Record):
     id: str = None
     transaction_time: str = None
@@ -14,6 +15,7 @@ class Card(faust.Record):
     response_code: str = None
     credit_name: str = None
     debit_name: str = None
+    account_id: str = None
 
 
 class Core(faust.Record):
@@ -23,5 +25,28 @@ class Core(faust.Record):
     occurrence_id: str = None
     status: str = None
     transaction_code: str = None
-    current_balance: str = None
+    current_balance: int = None
     transaction_date: str = None
+
+
+class CARD_CORE(faust.Record):
+    card_transaction_time: str = None
+    card_request_code: int = None
+    card_card_no: str = None
+    card_account_number: str = None
+    card_terminal_id: str = None
+    card_terminal_type: str = None
+    card_reference_id: str = None
+    card_amount: str = None
+    card_occurrence_id: str = None
+    card_response_code: str = None
+    card_credit_name: str = None
+    card_debit_name: str = None
+    core_id: str = None
+    core_transaction_type: str = None
+    core_account_id: str = None
+    core_occurrence_id: str = None
+    core_status: str = None
+    core_transaction_code: str = None
+    core_current_balance: str = None
+    core_transaction_date: str = None
